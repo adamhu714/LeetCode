563195c64b40601aa09289022f319910efc0ae82
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-04 23:48:02 +0000

    Time: 64 ms (90.27%), Space: 13.9 MB (94.39%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # permutation of string contains same number of each letter as each other
+        # sliding window across s2
+        # check if number of each letter is same as s1
+        
+        if len(s1) > len(s2):
+            return False
+        
+        window1 = [0]*26
+        window2 = [0]*26
+        
+        for i in range(len(s1)):
+            window1[ ord(s1[i])-ord('a') ] += 1
+            window2[ ord(s2[i])-ord('a') ] += 1
+       
+        if window1 == window2:
+            return True
+        
+        for i in range(len(s2)-len(s1)):
+            window2[ ord(s2[i])-ord('a') ] -= 1
+            window2[ ord(s2[i+len(s1)])-ord('a') ] += 1
+            if window1 == window2:
+                return True
+        return False
