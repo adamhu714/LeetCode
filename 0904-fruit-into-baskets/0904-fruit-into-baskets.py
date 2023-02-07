ad5aecc4e218e9e7d63dce68dfd6d4b12e94e643
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-07 02:27:07 +0000

    Time: 1081 ms (42.31%), Space: 20.2 MB (51.27%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,43 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        
+        # Want to find longest stretch of trees where there are only two types of tree
+        
+        # Sliding window across and store largest length of sliding window 
+        # before 3rd type of tree introduced
+        
+        
+        maxLength = 1
+        i = 0
+        j = 1
+        counter = 1
+        newI = 1
+        
+        a = fruits[0]
+        #b = fruits[]
+        
+        while j < len(fruits):
+            
+            if fruits[j] != a and fruits[j] != fruits[i]:
+                if a == fruits[i]:
+                    a = fruits[j]
+                    if j - i + 1 > maxLength:
+                        maxLength = j - i + 1
+                    newI = j
+                else:
+                    i = newI
+                    j = i
+                    a = fruits[i]
+                    newI += 1
+
+            else:
+                if fruits[newI] != fruits[j]:
+                    newI = j
+                if j - i + 1 > maxLength:
+                    maxLength = j - i + 1
+                
+            print(newI)
+            j += 1
+            
+        
+        return maxLength
