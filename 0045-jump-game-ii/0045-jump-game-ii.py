9bf1f10dfbe75d26a01e38724bffc80adaca6797
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-08 19:04:11 +0000

    Time: 125 ms (92.20%), Space: 15 MB (54.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,10 +1,5 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # want to return minimum number of jumps required
-        # know that we can reach the end, which means every index is reachable
-        # want to start from the beginning of the list, and exhaust every n jump route before looking at n+1 to prevent 
-        # redundant calculations 
-        # keep track of highest number reachable at every jump
         
         reached = 0
         oldReach = 0
