ef0b4c60d3e921a54b7bb9286a2291aee5a13ec5
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-08 17:29:31 +0000

    Time: 503 ms (61.84%), Space: 15.1 MB (78.83%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        '''
+        Function checks if you are able to reach the last index of the given array.
+        '''
+        reachable = 0
+        for i in range(len(nums)):
+            if i > reachable:
+                return False
+            reachable = max([i + nums[i], reachable])
+            if reachable >= len(nums) - 1:
+                return True
+        return True
