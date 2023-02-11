ce5393b2fd66336460c03f23fa454f7868ffd3d3
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-11 23:46:20 +0000

    Time: 122 ms (35.97%), Space: 19.9 MB (21.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,32 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        temp = {}
-        dictsForS = []
-        ansDict = {}
-        for i in range(len(strs)):
-            
-            for j in range(len(strs[i])):
-                
-                temp[strs[i][j]] = 1 + temp.get(strs[i][j], 0)
-                
-            if temp not in dictsForS:
-                dictsForS.append(temp)
-                ansDict[len(dictsForS)-1] = [strs[i]]
-            else:
-                idx = dictsForS.index(temp)
-                ansDict[idx].append(strs[i])
+        ans = collections.defaultdict(list)
 
-            temp = {}
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()                         
         
-        return list(ansDict.values())
+        
+#         temp = {}
+#         dictsForS = []
+#         ansDict = {}
+#         for i in range(len(strs)):
+            
+#             for j in range(len(strs[i])):
+                
+#                 temp[strs[i][j]] = 1 + temp.get(strs[i][j], 0)
+                
+#             if temp not in dictsForS:
+#                 dictsForS.append(temp)
+#                 ansDict[len(dictsForS)-1] = [strs[i]]
+#             else:
+#                 idx = dictsForS.index(temp)
+#                 ansDict[idx].append(strs[i])
+
+#             temp = {}
+        
+#         return list(ansDict.values())
