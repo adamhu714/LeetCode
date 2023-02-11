abe5959ff011b3bbd7339e3dc3d96cf9e8d87a70
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-11 23:51:50 +0000

    Time: 55 ms (56.17%), Space: 14.4 MB (61.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,13 +4,20 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        chrsS = {}
-        chrsT = {}
-
+        count1 = [0]*26
+        count2 = [0]*26
         
         for i in range(len(s)):
+            count1[ord(s[i]) - ord("a")] += 1
+            count2[ord(t[i]) - ord("a")] += 1   
+        return count1 == count2
+        
+#         chrsS = {}
+#         chrsT = {}
+        
+#         for i in range(len(s)):
             
-            chrsS[s[i]] = 1 + chrsS.get(s[i], 0)
-            chrsT[t[i]] = 1 + chrsT.get(t[i], 0)
+#             chrsS[s[i]] = 1 + chrsS.get(s[i], 0)
+#             chrsT[t[i]] = 1 + chrsT.get(t[i], 0)
             
-        return chrsS == chrsT
+#         return chrsS == chrsT
