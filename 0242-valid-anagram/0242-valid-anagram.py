8d191d819b1da0d0174d0e3c656c3e4e0b852e43
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-12 00:01:15 +0000

    Time: 47 ms (81.04%), Space: 14.5 MB (33.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+     
         if len(s) != len(t):
             return False
         
-        count1 = [0]*26
-        count2 = [0]*26
+        chrsS = {}
+        chrsT = {}
         
         for i in range(len(s)):
-            count1[ord(s[i]) - ord("a")] += 1
-            count2[ord(t[i]) - ord("a")] += 1   
-        return count1 == count2
+            
+            chrsS[s[i]] = 1 + chrsS.get(s[i], 0)
+            chrsT[t[i]] = 1 + chrsT.get(t[i], 0)
+            
+        return chrsS == chrsT
         
-#         chrsS = {}
-#         chrsT = {}
+
+        
+#         count1 = [0]*26
+#         count2 = [0]*26
         
 #         for i in range(len(s)):
-            
-#             chrsS[s[i]] = 1 + chrsS.get(s[i], 0)
-#             chrsT[t[i]] = 1 + chrsT.get(t[i], 0)
-            
-#         return chrsS == chrsT
+#             count1[ord(s[i]) - ord("a")] += 1
+#             count2[ord(t[i]) - ord("a")] += 1   
+#         return count1 == count2
+        
