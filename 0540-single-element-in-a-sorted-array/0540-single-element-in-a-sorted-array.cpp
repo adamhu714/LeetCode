0ea9546fd15d63339e1b7e9e285f93bf6eb4eca3
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-21 03:24:11 +0000

    Time: 34 ms (32.30%), Space: 22.4 MB (16.37%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        // sorted, therefore know that before target, 2n and 2n+1 index should be same
+        // also, 2n-1 should be different to 2n 
+        
+        if (nums.size() > 2 && nums[0] != nums[1]) {
+            return nums[0];
+        }
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = 0;
+        while (start != end) {
+            mid = ((start + end) / 2) - ((start + end) / 2) % 2;
+            if (nums[mid] == nums[mid + 1]) {
+                start = mid + 2;
+            } else if (nums[mid] == nums[mid-1]) {
+                end = mid - 2;
+            } else {
+                return nums[mid];
+            }
+        }
+        return nums[start];
+    }
+};
