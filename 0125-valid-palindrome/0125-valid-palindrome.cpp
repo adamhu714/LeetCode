cfd0605c9e7132ebbad708913145e158b7e7b6f9
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-22 21:10:32 +0000

    Time: 0 ms (100.00%), Space: 7.3 MB (93.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        s.erase(remove_if(s.begin(), s.end(), [](char c) {return !isalnum(c, locale());}), s.end());
-        transform(s.begin(), s.end(), s.begin(), ::toupper);
-        return equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
+    
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+        
+            while (isalnum(s[i]) == false && i < j) i++;
+            while (isalnum(s[j]) == false && i < j) j--;
+            
+            if (toupper(s[i]) != toupper(s[j])) return false; 
+        
+        }
+        
+        return true;
+    
     }
+    
 };
+
+
+    // bool isPalindrome(string s) {
+    //     s.erase(remove_if(s.begin(), s.end(), [](char c) {return !isalnum(c, locale());}), s.end());
+    //     transform(s.begin(), s.end(), s.begin(), ::toupper);
+    //     return equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
+    // }
