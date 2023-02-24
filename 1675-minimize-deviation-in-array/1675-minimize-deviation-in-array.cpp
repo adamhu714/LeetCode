80abda136945a598ab23ffee227ee946367223d2
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-24 19:33:09 +0000

    Time: 496 ms (68.64%), Space: 51.3 MB (100.00%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -4,20 +4,21 @@ public:
         
         int minN = INT_MAX;
         int res = INT_MAX;
-        priority_queue<int> pq;
         
         for (int& n : nums) {
             n = n % 2 ? n *= 2 : n;
             minN = min(minN, n);
-            pq.push(n);
         }
         
-        while (pq.top() % 2 == 0) {
-            res = min(res, pq.top() - minN);
-            minN = min(minN, pq.top()/2);
-            pq.push(pq.top()/2);
-            pq.pop();
+        make_heap(nums.begin(), nums.end());
+        
+        while (nums[0] % 2 == 0) {
+            res = min(res, nums[0] - minN);
+            minN = min(minN, nums[0]/2);
+            pop_heap(nums.begin(), nums.end());
+            nums.back()/=2;
+            push_heap(nums.begin(), nums.end());
         }
-        return min(res, pq.top() - minN);
+        return min(res, nums[0] - minN);
     }
 };
