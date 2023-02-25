66ddfd4aeaa9c414067f3adc115f0e56b82b7d56
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-25 04:53:12 +0000

    Time: 0 ms (100.00%), Space: 7.3 MB (80.65%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -4,13 +4,13 @@ public:
         vector<string> res(n);
         for (int i = 0; i < n; ++i) {
             if ((i+1) % 15 == 0) {
-                res[i] = "FizzBuzz";
+                res.at(i) = "FizzBuzz";
             } else if ((i+1) % 3 == 0) {
-                res[i] = "Fizz";
+                res.at(i) = "Fizz";
             } else if ((i+1) % 5 == 0) {
-                res[i] = "Buzz";
+                res.at(i) = "Buzz";
             } else {
-                res[i] = to_string(i+1);
+                res.at(i) = to_string(i+1);
             }
         }
         return res;
