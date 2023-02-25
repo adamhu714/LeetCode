66334442fde73cd09578ea2e796c3bb96150260e
Adam H <105497355+adamhu714@users.noreply.github.com>
2023-02-25 05:31:34 +0000

    Time: 8 ms (94.11%), Space: 8.9 MB (47.41%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        // if (ransomNote.size() > magazine.size()) return false;
+        if (ransomNote.size() > magazine.size()) return false;
         vector<int> vecMag(26, 0);
         for (char i : magazine) ++vecMag[i - 'a'];
         for (char i : ransomNote) if (--vecMag[i - 'a'] < 0) return false;
