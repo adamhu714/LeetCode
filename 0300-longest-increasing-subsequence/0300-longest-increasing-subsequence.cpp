746b637c979b74d46e69e7c982aefa91144dd068
Adam H <adamhu714@gmail.com>
2023-02-26 21:26:54 +0000

    Time: 9 ms (91.19%), Space: 10.4 MB (93.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        vector<int> LIS(1, nums[0]);
         int n = nums.size();
-        vector<int> dp(n, 1);
-        int maxVal = 1;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                    maxVal = max(maxVal, dp[i]);
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > LIS.back()) {
+                LIS.push_back(nums[i]);
+            } else {
+                // binary search
+                int start = 0, end = LIS.size() - 1, mid;
+                while (start < end) {
+                    mid = (start + end)/2;
+                    if (LIS[mid] >= nums[i]) {
+                        end = mid;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
+                if (LIS[start] > nums[i]) {
+                    LIS[start] = nums[i];
                 }
             }
         }
-        return maxVal;
+        return LIS.size();
     }
 };
+
+// dynamic programming:
+// time O(n^2)
+// space O(n)
+        // int n = nums.size();
+        // vector<int> dp(n, 1);
+        // int maxVal = 1;
+        // for (int i = 0; i < n; ++i) {
+        //     for (int j = 0; j < i; ++j) {
+        //         if (nums[j] < nums[i]) {
+        //             dp[i] = max(dp[i], dp[j] + 1);
+        //             maxVal = max(maxVal, dp[i]);
+        //         }
+        //     }
+        // }
+        // return maxVal;
