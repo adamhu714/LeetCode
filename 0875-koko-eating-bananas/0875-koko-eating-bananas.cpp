da6ad68734788f548eb1090684e6e038a3cb380b
Adam H <adamhu714@gmail.com>
2023-03-08 01:59:27 +0000

    Time: 78 ms (16.27%), Space: 18.9 MB (82.50%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool fastEnough(vector<int>& piles, int h, int k) {
+        int totalHours = 0;
+        for (int i = 0; i < piles.size(); ++i) {
+            totalHours += (piles[i] / k) + (piles[i] % k != 0);
+        }
+        return (totalHours <= h);
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int kr = *max_element(piles.begin(), piles.end());
+        int kl = 1;
+        int k = 0;
+        
+        while (kl < kr) {
+            k = (kl + kr) / 2;
+            if (fastEnough(piles, h, k)) {
+                kr = k;
+            } else {
+                kl = k + 1;
+            }
+        }
+        return kl;
+    }
+};
