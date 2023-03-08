cb613b2b7dc1df9aea734928d036bd1ace4692c0
Adam H <adamhu714@gmail.com>
2023-03-08 02:07:22 +0000

    Time: 41 ms (90.15%), Space: 18.7 MB (98.14%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool fastEnough(vector<int>& piles, int h, int k) {
+    bool fastEnough(const vector<int>& piles, const int& h, const int& k) {
         int totalHours = 0;
-        for (int i = 0; i < piles.size(); ++i) {
-            totalHours += (piles[i] / k) + (piles[i] % k != 0);
+        for (const auto pile : piles) {
+            totalHours += (pile / k) + (pile % k != 0);
         }
         return (totalHours <= h);
     }
-    int minEatingSpeed(vector<int>& piles, int h) {
+    int minEatingSpeed(vector<int>& piles, int& h) {
         int kr = INT_MAX/2; //*max_element(piles.begin(), piles.end());
         int kl = 1;
-        int k = 0;
+        int k;
         
         while (kl < kr) {
             k = (kl + kr) / 2;
