0bb1dc72d249a9455846d22587757aa22290247c
Adam H <adamhu714@gmail.com>
2023-03-20 05:20:55 +0000

    Time: 12 ms (93.73%), Space: 20.2 MB (76.11%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -26,7 +26,7 @@ public:
             }
         }
         
-        if (n == 0 || (n ==1 && flowerbed[flowerbed.size() - 2] + flowerbed[flowerbed.size() - 1] == 0)) {
+        if (n == 0 || (n == 1 && flowerbed[flowerbed.size() - 2] + flowerbed[flowerbed.size() - 1] == 0)) {
             return true;
         }
         
