6014ecc3759ab7e59448968b18bf343e7f475df8
Adam Hussain <adamhu714@gmail.com>
2024-07-09 21:46:31 +0100

    Time: 49 ms (58.04%), Space: 16.8 MB (76.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-     
+        
         if len(s) != len(t):
             return False
         
@@ -8,19 +8,8 @@ class Solution:
         chrsT = {}
         
         for i in range(len(s)):
-            
             chrsS[s[i]] = 1 + chrsS.get(s[i], 0)
             chrsT[t[i]] = 1 + chrsT.get(t[i], 0)
             
         return chrsS == chrsT
-        
-
-        
-#         count1 = [0]*26
-#         count2 = [0]*26
-        
-#         for i in range(len(s)):
-#             count1[ord(s[i]) - ord("a")] += 1
-#             count2[ord(t[i]) - ord("a")] += 1   
-#         return count1 == count2
         
